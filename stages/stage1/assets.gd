adfe23fea60319ae4b51e3ecefd0fad439c022ee
betaplusplus <97809970+betaplusplus@users.noreply.github.com>
2023-01-25 23:13:49 +0300

    init

---
+++ stages/stage1/assets.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+export var prepare_for_baking = false setget toggle_prepare
+
+func toggle_prepare(condition):
+	if condition == true:
+		for node in get_children():
+			if node is MeshInstance:
+				if node .mesh is ArrayMesh:
+					node.mesh = node.mesh.duplicate()
+					node.mesh.lightmap_unwrap(node.get.global_transform(), 0.05)
+					node.use_in_baked_light = true
+					node.mesh.lightmap_size_hint = node.mesh.lightmap_size_hint / 2
